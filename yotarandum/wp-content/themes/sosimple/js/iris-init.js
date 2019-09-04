3bd1a94872a03aa945b623790e3aacc1ff3bd014
yotaro <yotaro.genius@gmail.com>
2019-09-04 23:11:26 +0900

    ローカルに入れ替え 元データはBKフォルダへ 2

--- yotarandum/wp-content/themes/sosimple/js/iris-init.js
+++
@@ -1,9 +0,0 @@
-/**
- * Iris color picker custom settings
- *
- * Contains color picker for SoSimple theme options
- */
-
-jQuery(document).ready(function($){
- $('.color-picker').wpColorPicker();
-});
