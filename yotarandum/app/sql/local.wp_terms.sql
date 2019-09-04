8cf893f91cea87d7166d4b89514b0dbcd4f14be8
yotaro <yotaro.genius@gmail.com>
2019-09-05 00:04:51 +0900

    logs

--- yotarandum/app/sql/local.wp_terms.sql
+++ yotarandum/app/sql/local.wp_terms.sql
@@ -2,4 +2,20 @@
 /*!40014 SET FOREIGN_KEY_CHECKS=0*/;
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_terms` VALUES
-(1,"Uncategorized","uncategorized",0);
+(1,"未分類","uncategorized",0),
+(2,"パソコン","pc",0),
+(3,"デザイン","design",0),
+(4,"コーディング","coding",0),
+(5,"ツール&amp;サービス","tool_service",0),
+(6,"サーバー","server",0),
+(7,"ソフト&amp;アプリ","soft_app",0),
+(8,"ブラウザ","browser",0),
+(9,"サイト","site",0),
+(10,"ゲーム","game",0),
+(11,"まとめ、一覧","summary",0),
+(12,"パーツ","parts",0),
+(13,"プラグイン","plugin",0),
+(14,"用語","glossary",0),
+(15,"デバイス","device",0),
+(16,"スマートフォン","sp",0),
+(17,"WEBデザイン","web_design",0);
