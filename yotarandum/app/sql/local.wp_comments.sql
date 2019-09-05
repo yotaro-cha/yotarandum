4a5f15c65fef617faf16ee29bb1cfbe0908d0ae2
fukuzo <fukuzo.smile@gmail.com>
2019-09-05 23:53:40 +0900

    なんか・・・・・・

--- yotarandum/app/sql/local.wp_comments.sql
+++ yotarandum/app/sql/local.wp_comments.sql
@@ -2,4 +2,4 @@
 /*!40014 SET FOREIGN_KEY_CHECKS=0*/;
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_comments` VALUES
-(1,1,"A WordPress Commenter","wapuu@wordpress.example","https://wordpress.org/","","2019-08-26 13:20:49","2019-08-26 13:20:49","Hi, this is a comment.\nTo get started with moderating, editing, and deleting comments, please visit the Comments screen in the dashboard.\nCommenter avatars come from <a href=\"https://gravatar.com\">Gravatar</a>.",0,"1","","",0,0);
+(1,1,"A WordPress Commenter","wapuu@wordpress.example","https://wordpress.org/","","2019-09-04 14:40:20","2019-09-04 14:40:20","Hi, this is a comment.\r\nTo get started with moderating, editing, and deleting comments, please visit the Comments screen in the dashboard.\r\nCommenter avatars come from <a href=\"https://gravatar.com\">Gravatar</a>.",0,"1","","",0,0);
