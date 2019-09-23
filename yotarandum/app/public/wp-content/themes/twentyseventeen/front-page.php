319d900e171b129d30ada2272e72771f2dbd4327
yotaro <yotaro.genius@gmail.com>
2019-09-24 01:13:19 +0900

    tag.php途中 悩み中

--- yotarandum/app/public/wp-content/themes/twentyseventeen/front-page.php
+++ yotarandum/app/public/wp-content/themes/twentyseventeen/front-page.php
@@ -18,18 +18,119 @@ get_header(); ?>
 	<div class="l-contRow2">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+      <article id="post-<?php the_ID(); ?>" <?php post_class( 'twentyseventeen-panel ' ); ?> >
 
-			<?php
-			// Show the selected front page content.
-			if ( have_posts() ) :
-				while ( have_posts() ) :
-					the_post();
-					get_template_part( 'template-parts/page/content', 'front-page' );
-				endwhile;
-			else :
-				get_template_part( 'template-parts/post/content', 'none' );
-			endif;
-			?>
+        <section class="u-appsZone">
+          <span href="">
+            <img src="/wp-content/uploads/2018/10/icn_pc.png" alt="">
+            <p>パソコンの<br>基礎知識</p>
+          </span>
+          <span href="">
+            <img src="/wp-content/uploads/2018/10/icn_design.png" alt="">
+            <p>色、フォントや<br>レイアウトなど</p>
+          </span>
+          <a href="/?category_name=html">
+            <img src="/wp-content/uploads/2018/10/icn_html.png" alt="">
+            <p>html</p>
+          </a>
+          <a href="/?category_name=css">
+            <img src="/wp-content/uploads/2018/10/icn_css.png" alt="">
+            <p>css</p>
+          </a>
+          <a href="/?category_name=js">
+            <img src="/wp-content/uploads/2018/10/icn_js.png" alt="">
+            <p>javascript</p>
+          </a>
+          <span href="">
+            <img src="/wp-content/uploads/2018/10/icn_php.png" alt="">
+            <p>php</p>
+          </span>
+          <a href="/?category_name=wp">
+            <img src="/wp-content/uploads/2018/10/icn_wp.png" alt="">
+            <p>wordpress</p>
+          </a>
+          <span href="">
+            <img src="/wp-content/uploads/2018/10/icn_git.png" alt="">
+            <p>gitや<br>sourcetree</p>
+          </span>
+          <span href="">
+            <img src="/wp-content/uploads/2018/10/icn_server.png" alt="">
+            <p>FTPなど</p>
+          </span>
+          <span href="">
+            <img src="/wp-content/uploads/2018/10/icn_xl.png" alt="">
+            <p>excel</p>
+          </span>
+          <span href="">
+            <img src="/wp-content/uploads/2018/10/icn_ai.png" alt="">
+            <p>illustrator</p>
+          </span>
+          <span href="">
+            <img src="/wp-content/uploads/2018/10/icn_ps.png" alt="">
+            <p>photoshop</p>
+          </span>
+          <span href="">
+            <img src="/wp-content/uploads/2018/10/icn_dw.png" alt="">
+            <p>dreamweaver</p>
+          </span>
+          <span href="">
+            <img src="/wp-content/uploads/2018/10/icn_ability.png" alt="">
+            <p>webに関する<br>知識と技術</p>
+          </span>
+          <span href="">
+            <img src="/wp-content/uploads/2018/10/icn_term.png" alt="">
+            <p>調べた用語</p>
+          </span>
+          <span href="">
+            <img src="/wp-content/uploads/2018/10/icn_unknown.png" alt="">
+            <p>まだ調べてない用語</p>
+          </span>
+          <span href="">
+            <img src="/wp-content/uploads/2018/10/icn_wander.png" alt="">
+            <p>定期的に見た方が良いサイト</p>
+          </span>
+          <span href="">
+            <img src="/wp-content/uploads/2018/10/icn_game.png" alt="">
+            <p>ミニゲーム</p>
+          </span>
+          <a href="/?tag=summary">
+            <img src="/wp-content/uploads/2018/10/icn_summary.png" alt="">
+            <p>一覧のメモ</p>
+          </a>
+          <a href="/?tag=parts">
+            <img src="/wp-content/uploads/2018/10/icn_parts.png" alt="">
+            <p>コピペ用<br>コード集</p>
+          </a>
+          <span href="">
+            <img src="/wp-content/uploads/2018/10/icn_all_articles.png" alt="">
+            <p>全記事一覧</p>
+          </span>
+          <a href="/?page_id=49">
+            <img src="/wp-content/uploads/2018/10/icn_about.png" alt="">
+            <p>このサイトの説明</p>
+          </a>
+        </section>
+
+        <section class="newPosts">
+          <h2 class="u-ttl1"><img src="/wp-content/uploads/2018/10/txt_news_s.png" alt=""></h2>
+          <ul>
+
+            <?php
+            // Show the selected front page content.
+            if ( have_posts() ) :
+              while ( have_posts() ) :
+                the_post();
+                get_template_part( 'template-parts/page/content', 'front-page' );
+              endwhile;
+            else :
+              get_template_part( 'template-parts/post/content', 'none' );
+            endif;
+            ?>
+          </ul>
+        </section>
+
+
+      </article><!-- #post-## -->
 
 			<?php
 			// Get each of our panels and show the post data.
--- yotarandum/app/public/wp-content/themes/twentyseventeen/tag.php
+++ yotarandum/app/public/wp-content/themes/twentyseventeen/tag.php
@@ -15,59 +15,61 @@ get_header(); ?>
 <div class="l-inner">tag.php
 	<div class="l-contRow2">
     <div id="primary" class="content-area">
-    <?php if ( have_posts() ) : ?>
-      <header class="page-header">
-        <h1 class="pageTtl">札：<strong><?php single_tag_title(); ?></strong></h1>
-      </header><!-- .page-header -->
-    <?php endif; ?>
-
       <main id="main" class="site-main" role="main">
+        <article id="post-<?php the_ID(); ?>" <?php post_class( 'twentyseventeen-panel ' ); ?> >
 
-      <?php
-      if ( have_posts() ) :
-      ?>
-        <?php
-        /* Start the Loop */
-        while ( have_posts() ) :
-          the_post();
+          <section class="archivePosts">
+            <h1 class="u-ttl1">
+              <img src="/wp-content/uploads/2018/10/txt_tag_s.png" alt="">
+              <img src="/wp-content/uploads/<?php
+                $slug = get_queried_object()->slug;
+                if($slug == "css"){echo '2018/10/icn_css.png';}
+              ?>" alt="<?php single_tag_title(); ?>">
+              <!-- 
+                if文じゃなくcaseにする
+                tag.phpじゃなくarchive.phpにすべきか？
+              -->
+            </h1>
 
-          /*
-           * Include the Post-Format-specific template for the content.
-           * If you want to override this in a child theme, then include a file
-           * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-           */
-          //get_template_part( 'template-parts/post/content', get_post_format() );
-        ?>
+            <ul>
+            <?php
+            if ( have_posts() ) :
+            ?>
+            <?php
+            /* Start the Loop */
+            while ( have_posts() ) :
+              the_post();
+              get_template_part( 'template-parts/page/content', 'archive' );
 
-        <li>
-          <a href="<?php the_permalink(); ?>">
-            <h3 class="u-ttl2"><?php the_title(); ?></h3>
-            <?php $image = get_field('post_main'); if( empty($image) ): ?>
-              <div class="postMainImg"><img src="/wp-content/uploads/2018/10/noimg.png" alt="noimg" /></div>
-            <?php else: ?>
-              <div class="postMainImg"><img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" /></div>
-            <?php endif; ?>
-            <div class="excerpt"><?php the_excerpt(); ?></div>
-          </a>
-        </li>
+              /*
+               * Include the Post-Format-specific template for the content.
+               * If you want to override this in a child theme, then include a file
+               * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+               */
+              //get_template_part( 'template-parts/post/content', get_post_format() );
+            ?>
 
-        <?php
-        endwhile;
+            <?php
+            endwhile;
 
-        the_posts_pagination(
-          array(
-            'prev_text'          => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
-            'next_text'          => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
-            'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
-          )
-        );
+            the_posts_pagination(
+              array(
+                'prev_text'          => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+                'next_text'          => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+                'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+              )
+            );
 
-      else :
+          else :
 
-        get_template_part( 'template-parts/post/content', 'none' );
+            get_template_part( 'template-parts/post/content', 'none' );
 
-      endif;
-      ?>
+          endif;
+          ?>
+          </ul>
+        </section>
+
+      </article><!-- #post-## -->
 
       </main><!-- #main -->
     </div><!-- #primary -->
