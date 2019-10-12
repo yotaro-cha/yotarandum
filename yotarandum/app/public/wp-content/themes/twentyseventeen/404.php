32dfe276a816ead0369eca60cf755a48a252e2e5
yotaro <yotaro.genius@gmail.com>
2019-10-12 22:38:38 +0900

    404ページ途中

--- yotarandum/app/public/wp-content/themes/twentyseventeen/404.php
+++ yotarandum/app/public/wp-content/themes/twentyseventeen/404.php
@@ -17,16 +17,27 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentyseventeen' ); ?></h1>
-				</header><!-- .page-header -->
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyseventeen' ); ?></p>
+        <div class="sorryBox">
+          <div class="img"><img src="/wp-content/uploads/2019/10/yotaro_sorry.png" alt="ごめんなさい"></div>
+          <div class="txt">
+					  <h1 class="page-title">ごめんよぉ！</h1>
+            <p>お探しのページはこのサイトには無いんだよぉ。<br>
+            役立たずなオイラを許しておくれよぉ……</p>
+            <p>もし良かったら下のリンクから色々見てっとくれよぉ</p>
+            <p>本当にごめんよぉ……</p>
+          </div>
+        </div>
 
+				<div class="page-content">
 					<?php get_search_form(); ?>
 
+          <h2 class="u-ttl1"><img src="/wp-content/uploads/2018/10/txt_category_s.png" alt=""></h2>
+          <h2 class="u-ttl1"><img src="/wp-content/uploads/2018/10/txt_tag_s.png" alt=""></h2>
+          <h2 class="u-ttl1"><img src="/wp-content/uploads/2018/10/txt_archive_s.png" alt=""></h2>
 				</div><!-- .page-content -->
+
 			</section><!-- .error-404 -->
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 </div><!-- .l-inner -->
