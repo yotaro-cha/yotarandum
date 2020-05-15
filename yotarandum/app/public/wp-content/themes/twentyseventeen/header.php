a1163579e4c3d65969ba90e19484e8ccee4441be
yotaro <yotaro.genius@gmail.com>
2020-05-15 15:34:47 +0900

    アナリティクスの設定

--- yotarandum/app/public/wp-content/themes/twentyseventeen/header.php
+++ yotarandum/app/public/wp-content/themes/twentyseventeen/header.php
@@ -25,6 +25,15 @@
 
 <meta name="theme-color" content="#c5c56a">
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K6SDN5J');</script>
+<!-- End Google Tag Manager -->
+
+
 <?php wp_head(); ?>
 <link rel='stylesheet'  href='/wp-content/themes/twentyseventeen/mycustom/font/awesome.min.css' type='text/css' media='all' />
 <link rel='stylesheet'  href='/wp-content/themes/twentyseventeen/mycustom/css/plugin/prism.css' type='text/css' media='all' />
@@ -35,6 +44,12 @@
 </head>
 
 <body <?php body_class(); ?>>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K6SDN5J"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+
 <div class="hdColor">
 	<span class="blk"></span>
 	<span class="grn"></span>
